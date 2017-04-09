3ffedda6371c222f8e9ca9b5219081732315d420
citizenof17 <psokolllkow@mail.ru>
2017-04-09 12:36:37 +0400

    Bugs fixed, added comments

--- rb_tree.c
+++ rb_tree.c
@@ -26,6 +26,7 @@ Node* makeNewNode(Tree *T, int value)
   nd->left = nd->right = T->nil;
   nd->p = T->nil;
   nd->key = value;
+  nd->color = 'b';    //added
 
   return nd;
 }
@@ -103,24 +104,24 @@ void rightRotate(Tree *T, Node *x) //same as left rotate but all lefts changed t
 
 void insertFixup(Tree *T, Node *z)
 {
-  while (z->p->color == 'r'){
-    if (z->p == z->p->p->left){
-      Node *y = z->p->p->right;
-      if (y->color == 'r'){
-        z->p->color = 'b';
+  while (z->p->color == 'r'){   //while parent is red
+    if (z->p == z->p->p->left){ //case for left
+      Node *y = z->p->p->right; //uncle of z
+      if (y->color == 'r'){     //1# case, uncle is red 
+        z->p->color = 'b';      //change p's and uncles's color
         y->color = 'b';
-        z->p->p->color = 'r';
-        z = z->p->p;
+        z->p->p->color = 'r';   //change p's parent's color
+        z = z->p->p;            //do fixup for p's parent
       }else{
         if (z == z->p->right){
-          z = z->p;
-          leftRotate(T, z);
+          z = z->p;             //2# case, uncle is black
+          leftRotate(T, z);     // change z and parent
         }
-        z->p->color = 'b';
-        z->p->p->color = 'r';
-        rightRotate(T, z->p->p);
-      }
-    }else{
+        z->p->color = 'b';      //3# case,  change parent's color
+        z->p->p->color = 'r';   // change pp's color
+        rightRotate(T, z->p->p); //rotate, parent is now black node
+      } 
+    }else{                       //same for right
       Node *y = z->p->p->left;
       if (y->color == 'r'){
         z->p->color = 'b';
@@ -149,7 +150,7 @@ void insert(Tree *T, int val)  //rb-insert
 
   size += sizeof(z);
 
-  while (x != T->nil){
+  while (x != T->nil){ //looking for new place
     y = x;
     if (z->key < x->key)
       x = x->left;
@@ -157,24 +158,24 @@ void insert(Tree *T, int val)  //rb-insert
       x = x->right;
   }
 
-  z->p = y;
+  z->p = y;   //y is new parent
 
   if (y == T->nil)
-    T->root = z;
+    T->root = z;  //z is root
   else{
-    if (z->key < y->key)
+    if (z->key < y->key)  //is z left or right son?
       y->left = z;
     else
       y->right = z;
   }
-  z->left = T->nil;
-  z->right = T->nil;
-  z->color = 'r';
+  z->left = T->nil;  //no left child
+  z->right = T->nil; //no right child
+  z->color = 'r';    
 
   insertFixup(T, z);
 }
 
-void transplant(Tree *T, Node *u, Node *v)
+void transplant(Tree *T, Node *u, Node *v) //delete u, place v
 {
   if (u->p == T->nil)
     T->root = v;
@@ -187,34 +188,42 @@ void transplant(Tree *T, Node *u, Node *v)
   v->p = u->p;
 }
 
-void deleteFixup(Tree *T, Node *x)
+
+//we have extra "black"
+//properties will be restored if: 
+//#1 x is red-black node then we make it just black
+//or #2 x is root - make x just black
+//or #3 make rotations and coloring exit cycle
+void deleteFixup(Tree *T, Node *x) 
 {
   while (x != T->root  && x->color == 'b'){
-    if (x == x->p->left){
-      Node *w = x->p->right;
-      if (w->color == 'r'){
-        w->color = 'b';
-        x->p->color = 'r';
-        leftRotate(T, x->p);
-        w = x->p->right;
+    //x is always double blacked
+
+    if (x == x->p->left){  //if x is left son
+      Node *w = x->p->right;  //second x->parent's son
+      if (w->color == 'r'){  //case #1  -- brother is red
+        w->color = 'b';      //change brother's color
+        x->p->color = 'r';   //change parent's color
+        leftRotate(T, x->p);  //w is new pp for x
+        w = x->p->right;     //x's brother is black now
       }
-      if (w->left->color == 'b' && w->right->color == 'b'){
+      if (w->left->color == 'b' && w->right->color == 'b'){  //case #2
         w->color = 'r';
         x = x->p;
       }else{
-        if (w->right->color == 'b'){
+        if (w->right->color == 'b'){  //case #3 w is black, w's left son is red, w's right son is black
           w->left->color = 'b';
           w->color = 'r';
-          rightRotate(T, w);
-          w = x->p->right;
+          rightRotate(T, w);  //w-left is new x's brother
+          w = x->p->right;     //moved to 4th case
         }
-        w->color = x->p->color;
-        x->p->color = 'b';
+        w->color = x->p->color;    //case #4 w is black, w's right son is red
+        x->p->color = 'b';   
         w->right->color = 'b';
         leftRotate(T, x->p);
-        x = T->root;
+        x = T->root;  //End of cycle operation
       }
-    }else{
+    }else{   //same for right
       Node *w = x->p->left;
       if (w->color == 'r'){
         w->color = 'b';
@@ -245,21 +254,21 @@ void deleteFixup(Tree *T, Node *x)
 
 void delete(Tree *T, int val){
   Node *z = treeSearch(T, T->root, val);
-  Node *x = NULL;
+  Node *x = NULL;   //this node will replace y
   x = (Node*)malloc(sizeof(Node));
 
   if (z != T->nil){
-    Node *y = z;
-    char yColor = y->color;
-    if (z->left == T->nil){
-      x = z->right;
+    Node *y = z;                      //y "will be" deleted
+    char yColor = y->color;           //save y's color
+    if (z->left == T->nil){          //only lright son exists
+      x = z->right;              
       transplant(T, z, z->right);
     }else{
-      if (z->right == T->nil){
+      if (z->right == T->nil){         //only left son exists
         x = z->left;
         transplant(T, z, z->left);
-      }else{
-        y = treeMinimum(T, z->right);
+      }else{                          //both sons exist
+        y = treeMinimum(T, z->right); //find node to replace x
         yColor = y->color;
         x = y->right;
         if (y->p == z)
@@ -276,7 +285,7 @@ void delete(Tree *T, int val){
       }
     }
   
-    if (yColor == 'b')
+    if (yColor == 'b') //if y != z then replacing y Can lead to a violation of properties of rbt
       deleteFixup(T, x);
 
     size -= sizeof(z);
@@ -299,7 +308,6 @@ void delete(Tree *T, int val){
 
 long mtime()
 {
-  //http://dkhramov.dp.ua/Comp/TimeCount
   struct timeval t;
 
   gettimeofday(&t, NULL);
